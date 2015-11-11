9254ce590115b83aa88fd19c309a448482816c19
Denis Vengrin <denispo386@gmail.com>
2015-11-11 11:00:12 +0200

    Updated app logo

--- app/src/main/java/com/thinkmobiles/bodega/db/DBManager.java
+++ app/src/main/java/com/thinkmobiles/bodega/db/DBManager.java
@@ -55,10 +55,10 @@ public class DBManager {
         OrderItemDao orderItemDao = mDaoSession.getOrderItemDao();
         OrderItem orderItem = new OrderItem();
         orderItem.setName(itemWrapper.getName());
-        String iconPath = null;
+        /*String iconPath = null;
         if (itemWrapper.getProductList() != null && itemWrapper.getProductList().size() > 0)
-            iconPath = itemWrapper.getProductList().get(0).getImageSmall();
-        orderItem.setIcon(iconPath);
+            iconPath = itemWrapper.getProductList().get(0).getImageSmall();*/
+        orderItem.setIcon(orderItem.getIcon());
         orderItem.setPdf(itemWrapper.getPdf());
         orderItem.setCustomerId(customer.getId());
         orderItemDao.insert(orderItem);
